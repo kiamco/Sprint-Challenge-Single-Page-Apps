acf71ca388ad679a94569043333c41dae66dfc2f
kiamco <kiamcoo@gmail.com>
2019-08-17 09:14:17 -0700

    completed styling for nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import AppRouter from "./components/AppRouter.js";
+import Welcome from "./components/WelcomePage"
+import { BrowserRouter as Router, Route, NavLink} from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css'
+// import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
   return (
     <main>
       <Header />
       <TabNav />
-      <AppRouter />
+      <Route exact path='/' component={Welcome} />
+      {/* <AppRouter /> */}
     </main>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,36 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import {Tab, Menu, Icon, Divider} from "semantic-ui-react";
+import {NavLink} from "react-router-dom";
 
 // TODO: Add missing tabs below
 export default function TabNav() {
+    return (
+        <div class="ui menu">
+            <a class="item"> 
+                <NavLink exact className="home-page" to={'/'}>
+                    <Icon name='home' size='large' color='black' />
+                        Homepage
+                </NavLink>
+            </a>
+            <a class="item">
+                <NavLink exact className="characters">
+                    <Icon name='users' size='large' color='black' />
+                    Characters
+                </NavLink>
+            </a>
+            <a class="item">
+                <NavLink exact className="loaction">
+                    <Icon name='map' size='large' color='black' />
+                    Location
+                </NavLink>
+            </a>
+            <a class="item">
+                <NavLink exact className="episode">
+                    <Icon name='video camera' size='large' color='black' />
+                    Episode
+                </NavLink>
+            </a>
+        </div>
 
+    )
 };
